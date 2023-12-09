07ad092903082ade1b030e5fd2ee2319ff59851e
Rishabh Rawat <rishabh.rawat@smallcase.com>
2023-12-09 16:41:39 +0530

    fix: push protocol

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 3005,
   GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
   INFURA_API_KEY: process.env.INFURA_API_KEY,
+  WALLET_PRIVATE_KEY: process.env.WALLET_PRIVATE_KEY,
 }
--- src/loaders/pushProtocol.js
+++ src/loaders/pushProtocol.js
@@ -1,11 +1,12 @@
 const { PushAPI } = require('@pushprotocol/restapi');
 const {ethers } = require('ethers');
 
+const config = require('../../config');
 
 module.exports = {
   run: async () => {
     // Using random signer from a wallet, ideally this is the wallet you will connect
-    const signer = ethers.Wallet.createRandom();
+    const signer = ethers.Wallet(config.WALLET_PRIVATE_KEY);
 
     return PushAPI.initialize(signer, { env: 'staging' });
   }
