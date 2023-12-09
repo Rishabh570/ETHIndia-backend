4f8cc20c88d0ba06cba6a3d2fb4b0cdb805557c6
Rishabh Rawat <rishabh.rawat@smallcase.com>
2023-12-09 16:47:59 +0530

    fix: push protocol that's it

--- src/loaders/pushProtocol.js
+++ src/loaders/pushProtocol.js
@@ -6,7 +6,7 @@ const config = require('../../config');
 module.exports = {
   run: async () => {
     // Using random signer from a wallet, ideally this is the wallet you will connect
-    const signer = ethers.Wallet(config.WALLET_PRIVATE_KEY);
+    const signer = new ethers.Wallet(config.WALLET_PRIVATE_KEY);
 
     return PushAPI.initialize(signer, { env: 'staging' });
   }
